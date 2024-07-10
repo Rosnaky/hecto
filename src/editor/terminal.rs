474993ae5b57275864d5b34a59b6d75e5f10c900
Ronak Patel <ronakjpatelr22@gmail.com>
2024-07-09 23:18:09 -0400

    remvoed unused width parameter from size

--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,4 +1,4 @@
-use crossterm::cursor::{Hide, MoveTo, Show};
+use crossterm::cursor::{Hide, MoveTo};
 use crossterm::queue;
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType};
 use crossterm::style::Print;
@@ -6,7 +6,6 @@ use std::io::{stdout, Error, Write};
 
 #[derive(Copy, Clone)]
 pub struct Size {
-    pub width: u16,
     pub height: u16,
 }
 
@@ -40,14 +39,14 @@ impl Terminal {
         queue!(stdout(), Clear(ClearType::CurrentLine))?;
         Ok(())
     }
-    
+
     pub fn move_cursor_to(position: Position) -> Result<(), Error> {
         queue!(stdout(), MoveTo(position.x, position.y))?;
         Ok(())
     }
     pub fn size() -> Result<Size, Error> {
-        let (width, height) = size()?;
-        Ok(Size { width, height })
+        let (.., height) = size()?;
+        Ok(Size { height })
     }
 
     pub fn hide_cursor() -> Result<(), Error> {
