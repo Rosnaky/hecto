17504556fe976a01f61487add83a3a8055f029e5
Ronak Patel <ronakjpatelr22@gmail.com>
2024-07-10 18:40:19 -0400

    fixed unsafe errors

--- src/editor.rs
+++ src/editor.rs
@@ -3,13 +3,16 @@ mod terminal;
 use terminal::{Size, Position, Terminal};
 use std::io::Error;
 
+const NAME: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 pub struct Editor {
     should_quit: bool,
 }
 
 impl Editor {
     pub const fn default() -> Self {
-        return Self { should_quit: false }
+        Self { should_quit: false }
     }
     pub fn run(&mut self) {
         Terminal::initialize().unwrap();
@@ -27,7 +30,7 @@ impl Editor {
             let event = read()?;
             self.evaluate_event(&event);
         }
-        return Ok(())
+        Ok(())
     }
     fn evaluate_event(&mut self, event: &Event) {
         if let Key(KeyEvent {
@@ -53,17 +56,39 @@ impl Editor {
         }
         Terminal::show_cursor()?;
         Terminal::execute()?;
-        return Ok(())
+        Ok(())
     }
-    fn draw_rows() -> Result<(), std::io::Error> {
+    fn draw_welcome_message() -> Result<(), Error> {
+        let mut welcome_message = format!("{NAME} editor -- version {VERSION}", );
+
+        let width = Terminal::size()?.width as usize;
+        let len = welcome_message.len();
+
+        #[allow(clippy::integer_division)]
+        let padding = (width.saturating_sub(len)) / 2;
+        let spaces = " ".repeat(padding .saturating_sub(1));
+        welcome_message = format!("~{spaces}{welcome_message}");
+        welcome_message.truncate(width);
+        Terminal::print(&welcome_message)?;
+        Ok(())
+    }
+    fn draw_empty_row() -> Result<(), Error> {
+        Terminal::print("~")?;
+        Ok(())
+    }
+    fn draw_rows() -> Result<(), Error> {
         let Size{height, ..} = Terminal::size()?;
         for current_row in 0..height {
             Terminal::clear_line()?;
-            Terminal::print("~")?;
-            if current_row + 1 < height {
+            if current_row == height/3 {
+                Self::draw_welcome_message()?;
+            } else {
+                Self::draw_empty_row()?;
+            }
+            if current_row.saturating_add(1) < height {
                 Terminal::print("\r\n")?;
             }
         }
-        return Ok(())
+        Ok(())
     }
 }
--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,12 +1,14 @@
-use crossterm::cursor::{Hide, MoveTo};
-use crossterm::queue;
+use crossterm::cursor::{Hide, MoveTo, Show};
+use crossterm::{queue, Command};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType};
 use crossterm::style::Print;
 use std::io::{stdout, Error, Write};
+use core::fmt::Display;
 
 #[derive(Copy, Clone)]
 pub struct Size {
-    pub height: u16,
+    pub width: usize,
+    pub height: usize,
 }
 
 #[derive(Copy, Clone)]
@@ -21,51 +23,59 @@ impl Terminal {
     pub fn terminate() -> Result<(), Error> {
         Self::execute()?;
         disable_raw_mode()?;
-        return Ok(())
+        Ok(())
     }
     pub fn initialize() -> Result<(), Error> {
         enable_raw_mode()?;
         Self::clear_screen()?;
         Self::move_cursor_to(Position{x:0,y: 0})?;
         Self::execute()?;
-        return Ok(())
+        Ok(())
     }
     pub fn clear_screen() -> Result<(), Error> {
-        queue!(stdout(), Clear(ClearType::All))?;
-        return Ok(())
+        Self::queue_command(Clear(ClearType::All))?;
+        Ok(())
     }
 
     pub fn clear_line() -> Result<(), Error> {
-        queue!(stdout(), Clear(ClearType::CurrentLine))?;
-        return Ok(())
+        Self::queue_command(Clear(ClearType::CurrentLine))?;
+        Ok(())
     }
 
     pub fn move_cursor_to(position: Position) -> Result<(), Error> {
-        queue!(stdout(), MoveTo(position.x, position.y))?;
-        return Ok(())
+        Self::queue_command(MoveTo(position.x, position.y))?;
+        Ok(())
     }
     pub fn size() -> Result<Size, Error> {
-        let (.., height) = size()?;
-        return Ok(Size { height })
+        let (width_u16, height_u16) = size()?;
+        #[allow(clippy::as_conversions)]
+        let width = width_u16 as usize;
+        #[allow(clippy::as_conversions)]
+        let height = height_u16 as usize;
+        Ok(Size { width, height })
     }
 
     pub fn hide_cursor() -> Result<(), Error> {
         queue!(stdout(), Hide)?;
-        return Ok(())
+        Ok(())
     }
     
     pub fn show_cursor() -> Result<(), Error> {
-        queue!(stdout(), crossterm::cursor::Show)?;
-        return Ok(())
+        Self::queue_command(Show)?;
+        Ok(())
     }
     
-    pub fn print(string: &str) -> Result<(), Error> {
-        queue!(stdout(), Print(string))?;
-        return Ok(())
+    pub fn print<T: Display>(string: T) -> Result<(), Error> {
+        Self::queue_command(Print(string))?;
+        Ok(())
     }
 
     pub fn execute() -> Result<(), Error> {
         stdout().flush()?;
-        return Ok(())
+        Ok(())
+    }
+    fn queue_command<T: Command>(command: T) -> Result<(), Error> {
+        queue!(stdout(), command)?;
+        Ok(())
     }
 }
