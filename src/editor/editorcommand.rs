00fe6949d6e432b6d32242682c8c997ea58a72da
Ronak Patel <ronakjpatelr22@gmail.com>
2024-07-12 17:44:17 -0400

    added scrollable ability

---
+++ src/editor/editorcommand.rs
@@ -0,0 +1,52 @@
+use crossterm::event::{Event, KeyCode, KeyEvent, KeyModifiers};
+
+use super::terminal::Size;
+
+
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+    PageDown,
+    PageUp,
+    Home,
+    End,
+}
+
+pub enum EditorCommand {
+    Move(Direction),
+    Resize(Size),
+    Quit
+}
+
+impl TryFrom<Event> for EditorCommand {
+    type Error = String;
+
+    fn try_from(event: Event) -> Result<Self, Self::Error> {
+        match event {
+            Event::Key(KeyEvent {
+                code, modifiers, ..
+            }) => match (code, modifiers) {
+                (KeyCode::Char('q'), KeyModifiers::CONTROL) => Ok(Self::Quit),
+                (KeyCode::Up, KeyModifiers::NONE) => Ok(Self::Move(Direction::Up)),
+                (KeyCode::Down, KeyModifiers::NONE) => Ok(Self::Move(Direction::Down)),
+                (KeyCode::Left, KeyModifiers::NONE) => Ok(Self::Move(Direction::Left)),
+                (KeyCode::Right, KeyModifiers::NONE) => Ok(Self::Move(Direction::Right)),
+                (KeyCode::PageDown, KeyModifiers::NONE) => Ok(Self::Move(Direction::PageDown)),
+                (KeyCode::PageUp, KeyModifiers::NONE) => Ok(Self::Move(Direction::PageUp)),
+                (KeyCode::Home, KeyModifiers::NONE) => Ok(Self::Move(Direction::Home)),
+                (KeyCode::End, KeyModifiers::NONE) => Ok(Self::Move(Direction::End)),
+                _ => Err(format!("Unrecognized key: {:?}", code)),
+            },
+            Event::Resize(width_u16, height_u16) => {
+                #[allow(clippy::as_conversions)]
+                let height = height_u16 as usize;
+                #[allow(clippy::as_conversions)]
+                let width = width_u16 as usize;
+                Ok(Self::Resize(Size {height, width}))
+            }
+            _ => Err(format!("Unrecognized event: {:?}", event)),
+        }
+    }
+}
--- src/editor/view/buffer.rs
+++ src/editor/view/buffer.rs
@@ -1,10 +1,11 @@
 use std::{fs::read_to_string, io::Error};
+use super::line::Line;
 
 
 
 #[derive(Default)]
 pub struct Buffer {
-    pub lines: Vec<String>,
+    pub lines: Vec<Line>,
 }
 
 impl Buffer {
@@ -12,7 +13,7 @@ impl Buffer {
         let file_contents = read_to_string(file_name)?;
         let mut lines = Vec::new();
         for line in file_contents.lines() {
-            lines.push(String::from(line));
+            lines.push(Line::from(line));
         }
         Ok(Self { lines })
     }
